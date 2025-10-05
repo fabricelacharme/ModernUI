17095c53fe7c00f58d7c106543209fc789524ac5
Fabrice Lacharme <fabrice.lacharme@gmail.com>
2025-10-05 18:12:37 +0200

    reduce size of fonts of submenus

--- ModernUI/frmMain.cs
+++ ModernUI/frmMain.cs
@@ -107,7 +107,7 @@ namespace ModernUI
                         Color color = Color.LightBlue;
                         currentButton.BackColor = color;
                         currentButton.ForeColor = Color.White;
-                        currentButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+                        currentButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
                         
                     }
                 }
