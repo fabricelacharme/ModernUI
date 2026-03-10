139507134e39d9cc8efca4a75f90830c3839b470
Fabrice Lacharme <fabrice.lacharme@gmail.com>
2026-03-10 13:57:05 +0100

    fix

--- ModernUI/Program.cs
+++ ModernUI/Program.cs
@@ -16,8 +16,7 @@ namespace ModernUI
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmMain());           
-            //Application.Run(new Form1());
+            Application.Run(new frmMain());                       
         }
     }
 }
